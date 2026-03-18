0a6e13616082079fd776dae697d6f3dc3429beff
swilam <m.swilam@storeus.com>
2026-03-18 15:55:24 +0200

    Initial Commit

---
+++ swiftBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftBootCamp
+//
+//  Created by Swilam on 18/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftBootCamp/swiftBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftBootCampApp.swift
+//  swiftBootCamp
+//
+//  Created by Swilam on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct swiftBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
